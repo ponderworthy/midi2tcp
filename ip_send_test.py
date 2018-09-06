cf9a7b5545abbd642cbdc7132ca9ecbfde6f1a6b
Jonathan E. Brickman <jeb@ponderworthy.com>
2018-09-06 07:14:00 -0500

    Delete ip_send_test.py

--- ip_send_test.py
+++
@@ -1,8 +0,0 @@
-
-import sys
-import mido
-from mido.sockets import PortServer, connect
-
-output = connect('localhost', 44440)
-message = mido.Message('note_on', note=100, velocity=3, time=1)
-output.send(message)
